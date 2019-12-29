cfad2f0dd68c009434578a602050303485f7bca4
wcyyyy <873485523@qq.com>
2019-12-29 15:50:46 +0800

    班级管理

---
+++ src/main/java/xyz/lightseekers/maven_school/service/impl/ClazzServiceImpl.java
@@ -0,0 +1,79 @@
+package xyz.lightseekers.maven_school.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import xyz.lightseekers.maven_school.bean.Clazz;
+import xyz.lightseekers.maven_school.bean.ClazzExample;
+import xyz.lightseekers.maven_school.bean.ex.ClazzEx;
+import xyz.lightseekers.maven_school.mapper.ClazzMapper;
+import xyz.lightseekers.maven_school.mapper.ex.ClazzExMapper;
+import xyz.lightseekers.maven_school.service.IClazzService;
+import xyz.lightseekers.maven_school.util.DaoUtil;
+
+import java.util.List;
+@Service
+public class ClazzServiceImpl implements IClazzService {
+
+    @Autowired
+    private ClazzMapper clazzMapper;
+    @Autowired
+    private ClazzExMapper clazzExMapper;
+
+    @Override
+    public String deleteById(int id) throws RuntimeException {
+        return DaoUtil.messageString(clazzMapper.deleteByPrimaryKey(id),DaoUtil.DELETE);
+    }
+
+    @Override
+    public String addOrUpdate(Clazz clazz) throws RuntimeException {
+        if (clazz==null) throw new RuntimeException();
+        if(clazz.getId()==null){
+            clazz.setServeNumber(0);
+            return DaoUtil.messageString(clazzMapper.insert(clazz),DaoUtil.INSERT);
+        }else{
+            return DaoUtil.messageString(clazzMapper.updateByPrimaryKey(clazz),DaoUtil.UPDATE);
+        }
+    }
+
+    @Override
+    public String deleteMany(int id[]) throws RuntimeException {
+        for (int i=0;i<id.length;i++){
+            deleteById(id[i]);
+        }
+        return DaoUtil.messageString(id.length,DaoUtil.DELETE);
+    }
+
+
+    @Override
+    public List<ClazzEx> findAll() throws RuntimeException {
+        List<ClazzEx> all = clazzExMapper.findAll();
+        return all;
+    }
+
+    @Override
+    public ClazzEx findById(int id) throws RuntimeException {
+        return clazzExMapper.findById(id);
+    }
+
+    @Override
+    public List<ClazzEx> findByCondi(String key, String word) throws RuntimeException {
+        if((key==null||"".equals(key))&&(word==null||"".equals(word))){
+            return clazzExMapper.findAll();
+        }else if((key==null||"".equals(key))&&!"".equals(word)){
+            word= word==null?"":word;
+            word="%"+word+"%";
+            return clazzExMapper.findByNameOrTeacher(word);
+        }else if("name".equals(key)){
+            word= word==null?"":word;
+            word="%"+word+"%";
+            return clazzExMapper.findByName(word);
+        }else if ("teacher".equals(key)){
+            word= word==null?"":word;
+            word="%"+word+"%";
+            return clazzExMapper.findByTeacher(word);
+        }
+        return null;
+    }
+
+
+}
---
+++ src/main/java/xyz/lightseekers/maven_school/web/controller/ClazzController.java
@@ -0,0 +1,70 @@
+package xyz.lightseekers.maven_school.web.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import xyz.lightseekers.maven_school.bean.Clazz;
+import xyz.lightseekers.maven_school.mapper.ex.ClazzExMapper;
+import xyz.lightseekers.maven_school.service.IClazzService;
+import xyz.lightseekers.maven_school.util.Message;
+import xyz.lightseekers.maven_school.util.MessageUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@Api(description = "班级管理")
+@RequestMapping("clazz")
+public class ClazzController {
+
+    @Autowired
+    private IClazzService clazzService;
+
+    @ApiOperation("查询所有班级")
+    @GetMapping("/findAll")
+    public Message findAll(){
+
+        return MessageUtil.success(clazzService.findAll());
+    }
+    @ApiOperation("新增班级")
+    @PostMapping("/add")
+    public Message add(Clazz clazz){
+        return MessageUtil.success(clazzService.addOrUpdate(clazz));
+    }
+    @ApiOperation("修改班级")
+    @PostMapping("/update")
+    public Message update(Clazz clazz){
+        return MessageUtil.success(clazzService.addOrUpdate(clazz));
+    }
+    @ApiOperation("根据id删除班级")
+    @GetMapping("/deleteById")
+    public Message deleteById(int id){
+        return MessageUtil.success(clazzService.deleteById(id));
+    }
+    @ApiOperation("批量删除")
+    @GetMapping("/deleteMany")
+    public Message deleteMany(int id[]){
+        return MessageUtil.success(clazzService.deleteMany(id));
+    }
+
+    @GetMapping("/findById")
+    @ApiOperation("根据id查找班级")
+    public Message findById(int id){
+        return MessageUtil.success(clazzService.findById(id));
+    }
+
+    @GetMapping("/search")
+    @ApiOperation("条件搜索")
+    public Message findByNameOrTeacher(String key,String word){
+        return MessageUtil.success(clazzService.findByCondi(key,word));
+    }
+
+
+
+
+
+}
