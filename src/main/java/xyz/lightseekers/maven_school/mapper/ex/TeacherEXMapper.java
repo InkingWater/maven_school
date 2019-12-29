2bdae3cd939fffd63ebd2c601949101d0215a5af
wangqr <594684736@qq.com>
2019-12-29 19:42:38 +0800

    wqr修改搜索栏

--- src/main/java/xyz/lightseekers/maven_school/mapper/ex/TeacherEXMapper.java
+++ src/main/java/xyz/lightseekers/maven_school/mapper/ex/TeacherEXMapper.java
@@ -1,13 +1,21 @@
 package xyz.lightseekers.maven_school.mapper.ex;
 
+import org.springframework.stereotype.Repository;
 import xyz.lightseekers.maven_school.bean.ex.TeacherEX;
 
 import java.util.List;
-
+@Repository
 public interface TeacherEXMapper {
     //搜索栏 空+不空
-    List<TeacherEX> selectGender(String word) throws RuntimeException;
+    List<TeacherEX> selectByGender(String word) throws RuntimeException;
 
+    /**
+     * 搜索栏通过下拉栏来进行搜索，只有一个gender的值
+     * @param word
+     * @return
+     * @throws RuntimeException
+     */
+    List<TeacherEX> selectGender(String word) throws RuntimeException;
     //搜索栏 空+空
     List<TeacherEX> selectAll() throws RuntimeException;
 }
