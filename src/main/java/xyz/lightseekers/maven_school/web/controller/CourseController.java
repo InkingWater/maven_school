766ebdae493ab68dd5dd08a50a453f9894b2f6a3
MJX99 <399895708@qq.com>
2019-12-30 17:25:30 +0800

    Add

--- src/main/java/xyz/lightseekers/maven_school/web/controller/CourseController.java
+++ src/main/java/xyz/lightseekers/maven_school/web/controller/CourseController.java
@@ -27,7 +27,7 @@ public class CourseController {
 
     @GetMapping("/selectAll")
     @ApiOperation(value = "查找所有")
-    public Message selectAll(){
+    public Message selectAll(){                                  //查找所有的课程并返回
         List<Course> list = iCourseService.findAll();
         return MessageUtil.success(list);
     }
@@ -35,35 +35,36 @@ public class CourseController {
 
     @GetMapping("/searchByCon")
     @ApiOperation(value = "根据名字或描述查找")
-    public Message searchByCon(String key, String word){
+    public Message searchByCon(String key, String word){        //根据名字或描述查找课程
         List<Course> list = iCourseService.search(key,word);
         return MessageUtil.success(list);
     }
 
     @GetMapping("/deleteById")
     @ApiOperation(value = "根据id删除")
-    public Message deleteById(int id){
+    public Message deleteById(int id){                            //调用service中的方法删除
         iCourseService.deleteById(id);
         return MessageUtil.success("操作成功");
     }
 
     @PostMapping("/add")
     @ApiOperation(value = "插入")
-    public Message add(Course course){
+    public Message add(Course course){                      //插入新的课程对象
         iCourseService.insertOrUpdate(course);
         return MessageUtil.success(course);
     }
 
     @PostMapping("/update")
     @ApiOperation(value = "更新")
-    public Message update(Course course){
+    public Message update(Course course){               //更新新的课程
         iCourseService.insertOrUpdate(course);
         return MessageUtil.success(course);
     }
 
+
     @ApiOperation("批量删除")
     @GetMapping("/deleteBatch")
-    public Message deleteBatch(int id[]){
+    public Message deleteBatch(int id[]){               //对别选择的课程通过id批量删除
         return MessageUtil.success(iCourseService.deleteBatch(id));
     }
 }
