16478314684b1052eebeffdb98765d2bccb45355
wcyyyy <873485523@qq.com>
2019-12-31 15:13:54 +0800

    课调管理

--- src/main/java/xyz/lightseekers/maven_school/service/ISurverManageService.java
+++ src/main/java/xyz/lightseekers/maven_school/service/ISurverManageService.java
@@ -1,7 +1,18 @@
 package xyz.lightseekers.maven_school.service;
 
 import xyz.lightseekers.maven_school.bean.Survey;
+import xyz.lightseekers.maven_school.bean.ex.SurverManageEx;
+
+import java.util.List;
 
 public interface ISurverManageService  {
     String  addOrUpdate(Survey survey) throws RuntimeException;
+    String deleteById(int id) throws  RuntimeException;
+    String deleteMany(int id[])throws RuntimeException;
+
+    List<SurverManageEx> findAll() throws RuntimeException;
+    SurverManageEx findASurverById(int id) throws RuntimeException;
+
+    List<SurverManageEx> search(String word) throws RuntimeException;
+
 }
